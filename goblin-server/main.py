9f844359a8a3b27c34b9c8c827bd46ce927baceb
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-11-22 01:46:09 +0100

    allow all CORS on goblin-server

--- goblin-server/main.py
+++ goblin-server/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import request
-import os,time
+from flask_cors import CORS, cross_origin
+import os
 
 app = Flask(__name__)
+cors = CORS(app)
 
 file_path = os.path.realpath(__file__)
 levels_path = os.path.join(os.path.abspath(os.path.join(file_path, os.pardir)),"levels")
@@ -11,12 +13,15 @@ if not os.path.isdir(levels_path):
     os.mkdir(levels_path)
 
 @app.route("/")
+@cross_origin()
 def home():
     return "<p>hello! this is the backend server for my videogame called 'Goblin Maker'</p>\n<p>you can try it <a href='https://github.com/ingobeans/goblin-maker'>here</a></p>"
 @app.route("/list")
+@cross_origin()
 def list():
     return ",".join(os.listdir(levels_path))
 @app.route("/get/<id>")
+@cross_origin()
 def get(id: str):
     if not all([c.isalnum() or c == ' ' for c in id]):
         return "invalid :("
@@ -26,6 +31,7 @@ def get(id: str):
     with open(path,"r") as f:
         return f.read()
 @app.route("/upload/<id>")
+@cross_origin()
 def upload(id: str):
     if not all([c.isalnum() or c == ' ' for c in id]):
         return "invalid :("
